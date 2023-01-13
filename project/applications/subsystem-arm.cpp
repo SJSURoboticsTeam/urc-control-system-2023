b98b065a902e6f7449d4bbe4e03fbfbd0c4b8689
corey kelley <coreyboy1820@hotmail.com>
2023-01-13 00:16:38 -0800

    Testing libhal hello world in new hardware map

--- project/applications/subsystem-arm.cpp
+++ project/applications/subsystem-arm.cpp
@@ -1,11 +1,16 @@
+#include <libhal-util/serial.hpp>
 #include <libhal-util/steady_clock.hpp>
 
 #include "../hardware_map.hpp"
 
 hal::status application(Drive::hardware_map& p_map)
 {
-    while(true) {
 
+    using namespace std::chrono_literals;
+    using namespace hal::literals;
+    while(true) {
+    HAL_CHECK(hal::write(*p_map.terminal, "Hello, World!\n"));
+    HAL_CHECK(hal::delay(*p_map.steady_clock, 1000ms));
     }
 
   return hal::success();
