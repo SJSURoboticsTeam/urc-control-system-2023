a90436932658eeb2b2677c4cc483c14b36f388b7
corey kelley <coreyboy1820@hotmail.com>
2023-01-25 20:05:23 -0800

    trying to test rmd-encoder

--- drive/applications/app.cpp
+++ drive/applications/app.cpp
@@ -30,7 +30,7 @@ hal::status application(drive::hardware_map& p_map)
   using namespace std::chrono_literals;
   using namespace hal::literals;
 
-  auto& esp = *p_map.esp;
+  // auto& esp = *p_map.esp;
   auto& terminal = *p_map.terminal;
   auto& counter = *p_map.steady_clock;  // check
   auto& magnet0 = *p_map.in_pin0;
@@ -38,39 +38,39 @@ hal::status application(drive::hardware_map& p_map)
   auto& magnet2 = *p_map.in_pin2;  //
   auto& can = *p_map.can;          // check
 
-  std::array<hal::byte, 8192> buffer{};
-  static std::string_view get_request = "";
+  // std::array<hal::byte, 8192> buffer{};
+  // static std::string_view get_request = "";
 
-  HAL_CHECK(hal::write(terminal, "Starting program...\n"));
+  // HAL_CHECK(hal::write(terminal, "Starting program...\n"));
 
-  auto wifi_result = hal::esp8266::at::wlan_client::create(
-    esp,
-    "SJSU Robotics 2.4GHz",
-    "R0Bot1cs3250",
-    HAL_CHECK(hal::create_timeout(counter, 10s)));
+  // auto wifi_result = hal::esp8266::at::wlan_client::create(
+  //   esp,
+  //   "SJSU Robotics 2.4GHz",
+  //   "R0Bot1cs3250",
+  //   HAL_CHECK(hal::create_timeout(counter, 10s)));
 
-  if (!wifi_result) {
-    HAL_CHECK(hal::write(terminal, "Failed to create wifi client!\n"));
-    return wifi_result.error();
-  }
+  // if (!wifi_result) {
+  //   HAL_CHECK(hal::write(terminal, "Failed to create wifi client!\n"));
+  //   return wifi_result.error();
+  // }
 
-  auto wifi = wifi_result.value();
+  // auto wifi = wifi_result.value();
 
-  auto socket_result = hal::esp8266::at::socket::create(
-    wifi,
-    HAL_CHECK(hal::create_timeout(counter, 1s)),
-    {
-      .type = hal::socket::type::tcp,
-      .domain = "192.168.1.183",
-      .port = "5000",
-    });
+  // auto socket_result = hal::esp8266::at::socket::create(
+  //   wifi,
+  //   HAL_CHECK(hal::create_timeout(counter, 1s)),
+  //   {
+  //     .type = hal::socket::type::tcp,
+  //     .domain = "192.168.56.1",
+  //     .port = "5000",
+  //   });
 
-  if (!socket_result) {
-    HAL_CHECK(hal::write(terminal, "TCP Socket couldn't be established\n"));
-    return socket_result.error();
-  }
+  // if (!socket_result) {
+  //   HAL_CHECK(hal::write(terminal, "TCP Socket couldn't be established\n"));
+  //   return socket_result.error();
+  // }
 
-  auto socket = std::move(socket_result.value());
+  // auto socket = std::move(socket_result.value());
 
   auto can_router = hal::can_router::create(can).value();
 
@@ -102,41 +102,40 @@ hal::status application(drive::hardware_map& p_map)
   Drive::CommandLerper lerp;
 
   HAL_CHECK(hal::delay(counter, 1000ms));
-  // tri_wheel.HomeLegs(terminal);
+  tri_wheel.HomeLegs(terminal);
   HAL_CHECK(hal::delay(counter, 1000ms));
   HAL_CHECK(hal::write(terminal, "Starting control loop..."));
 
   while (true) {
-    buffer.fill('.');
-    get_request = "GET /drive" + get_rover_status() +
-                  " HTTP/1.1\r\n"
-                  "Host: 192.168.1.183:5000/\r\n"
-                  "\r\n";
+    // buffer.fill('.');
+    // get_request = "GET /drive" + get_rover_status() +
+    //               " HTTP/1.1\r\n"
+    //               "Host: 192.168.56.1:5000/\r\n"
+    //               "\r\n";
 
-    auto write_result =
-      socket.write(hal::as_bytes(get_request),
-                   HAL_CHECK(hal::create_timeout(counter, 500ms)));
-    if (!write_result) {
-      continue;
-    }
+    // auto write_result =
+    //   socket.write(hal::as_bytes(get_request),
+    //                HAL_CHECK(hal::create_timeout(counter, 500ms)));
+    // if (!write_result) {
+    //   continue;
+    // }
 
-    HAL_CHECK(hal::delay(counter, 100ms));
+    // HAL_CHECK(hal::delay(counter, 100ms));
 
-    auto received = HAL_CHECK(socket.read(buffer)).data;
+    // auto received = HAL_CHECK(socket.read(buffer)).data;
 
-    auto result = to_string_view(received);
+    // auto result = to_string_view(received);
 
-    auto start = result.find('{');
-    auto end = result.find('}');
-    auto json = result.substr(start, end - start + 1);
+    // auto start = result.find('{');
+    // auto end = result.find('}');
+    // auto json = result.substr(start, end - start + 1);
 
-    HAL_CHECK(hal::write(terminal, json));
-    HAL_CHECK(hal::write(terminal, "\r\n\n"));
+    // HAL_CHECK(hal::write(terminal, json));
+    // HAL_CHECK(hal::write(terminal, "\r\n\n"));
 
-    std::string json_string(json);
-
-    commands =
-      HAL_CHECK(mission_control.ParseMissionControlData(json_string, terminal));
+    // std::string json_string(json);
+    // commands =
+    //   HAL_CHECK(mission_control.ParseMissionControlData(json_string, terminal));
 
     commands = rules_engine.ValidateCommands(commands);
     commands = mode_switch.SwitchSteerMode(commands, arguments, motor_speeds);
--- drive/soft-driver/rmd-encoder.hpp
+++ drive/soft-driver/rmd-encoder.hpp
@@ -11,17 +11,16 @@ public:
   static float CalcEncoderPositions(hal::rmd::drc& motor, float type)
   {
     using namespace hal::literals;
-    hal::rmd::drc::read read_commands{};
-    motor.feedback_request(read_commands);
+    motor.feedback_request(hal::rmd::drc::read::encoder_data);
     int eight_bit_raw_encoder_data =
-      static_cast<float>(motor.feedback().encoder >> 8);
+      static_cast<float>(motor.feedback().encoder);
     // We shift right because the driver returns this value as a 16-bit number
     // but the datasheet says its an 8 bit number
     return MapEncoderDataToDegrees(eight_bit_raw_encoder_data, type);
   }
 
 private:
-  static float MapEncoderDataToDegrees(int8_t encoder_data, float type)
+  static float MapEncoderDataToDegrees(int16_t encoder_data, float type)
   {
     // we use a float here because we need decimal precision
     float temp = (type * encoder_data / 256.0f);
