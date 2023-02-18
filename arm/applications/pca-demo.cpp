6ac8597c331151c9c4ae53008776967b407a32fb
Alexander Pereira <xanderpereiraj@gmail.com>
2023-02-18 15:53:40 -0800

    :construction: Added coreys rr9 driver to working PCA demo

--- arm/applications/pca-demo.cpp
+++ arm/applications/pca-demo.cpp
@@ -6,6 +6,7 @@
 #include "../implementation/mission-control-handler.hpp"
 
 #include "../hardware_map.hpp"
+#include "../implementation/routers/rr9-router.hpp"
 
 #include <string>
 #include <string_view>
@@ -24,6 +25,7 @@ hal::status application(drive::hardware_map& p_map)
   hal::print(console, "[pca9685] Application Starting...\n\n");
   auto pca9685 = HAL_CHECK(hal::pca::pca9685::create(i2c, 0b100'0000));
   auto pwm0 = pca9685.get_pwm_channel<0>();
+  Arm::RR9Router RR9(pca9685);
 
   HAL_CHECK(pwm0.frequency(50.0_Hz));
   hal::print(console, "pca9685 Application Starting...\n\n");
---
+++ arm/implementation/routers/rr9-router.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <libhal-util/units.hpp>
+#include "../../dto/arm-dto.hpp"
+#include <libhal-pca/pca9685>
+#include <libhal-util/include/libhal-util/map>
+
+namespace Arm
+{
+    class RR9Router
+    {
+    public:
+
+        RR9Router(hal::pca::pca9685 pca) : pca_(pca)
+        {
+            pwm0_ = pca.get_pwm_channel<0>();
+            HAL_CHECK(pwm0_.frequency(50_Hz));
+        }
+
+        mc_commands SetRR9Arguments(mc_commands args)
+        {
+            // do logic for RR9 moving here
+            std::pair<float, float> from;
+            std::pair<float, float> to;
+            from.first = 0;
+            from.second = 180;
+            to.first = 0.025f;
+            to.second = 0.125f;
+            duty_cycle = hal::map(args.rr9_angle, from, to);
+            HAL_CHECK(pwm0_.duty_cycle(duty_cycle));
+            return args;
+        }
+
+    private:
+        hal::pca::pca9685 pca_;
+        hal::pca::pca9685::pwm_channel pwm0_;
+    };
+}
