2adc7f52737f1654850dedddbd2ea581ce6dbc15
Nathanael Garza <nate.garza@hotmail.com>
2023-02-18 16:13:52 -0800

    cleans up demo - xander

--- arm/applications/pca-demo.cpp
+++ arm/applications/pca-demo.cpp
@@ -34,23 +34,14 @@ hal::status application(drive::hardware_map& p_map)
 
   HAL_CHECK(hal::write(console, "Starting program new...\n"));
 
-  hal::print(console, "[pca9685] Application Starting...\n\n");
   auto pca9685 = HAL_CHECK(hal::pca::pca9685::create(i2c, 0b100'0000));
   auto pwm0 = pca9685.get_pwm_channel<0>();
-  // Arm::RR9Router RR9(pca9685);
 
   HAL_CHECK(pwm0.frequency(50.0_Hz));
   hal::print(console, "pca9685 Application Starting...\n\n");
 
-  HAL_CHECK(hal::write(console, "first step...\n"));
-  HAL_CHECK(pwm0.duty_cycle(.0255f));
-
-  HAL_CHECK(hal::delay(clock, 2s));
-  HAL_CHECK(hal::write(console, "second step...\n"));
-  HAL_CHECK(pwm0.duty_cycle(0.13f));
-  HAL_CHECK(hal::delay(clock, 2s));
-
   while (true) {
+    // Sweep from 0 to 180 degrees
     // for (int i = 0; i < 180; i++) {
     //   HAL_CHECK(pwm0.duty_cycle(convert_to_duty_cycle(i)));
     //   HAL_CHECK(hal::delay(clock, 100ms));
