7472a9440cef483e0b445a9e42e4c38e84704f86
Nathanael Garza <41933236+naterpotatoers@users.noreply.github.com>
2023-02-20 19:40:56 -0800

    adds serial demo file with just basic functionality, cleaned up Evans implementation (#45)

---
+++ drive/implementation/serial.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <libhal/serial.hpp>
+
+#include <string_view>
+
+namespace Drive {
+inline hal::result<std::string_view> GetCommands(hal::serial& terminal)
+{
+  std::array<hal::byte, 8192> buffer{};
+  auto received = HAL_CHECK(terminal.read(buffer)).data;
+  if (received.size() != 0) {
+    auto response = std::string_view(
+      reinterpret_cast<const char*>(received.data()), received.size());
+    hal::print<200>(
+      terminal, "%.*s", static_cast<int>(response.size()), response.data());
+    return response;
+  } else {
+    return std::string_view("");
+  }
+}
+}  // namespace Drive
