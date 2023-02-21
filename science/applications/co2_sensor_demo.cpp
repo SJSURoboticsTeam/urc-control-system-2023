e76f9ecca1cf82e7a91d657c1932c08d9ef38517
Viha123 <sviha195@gmail.com>
2023-02-20 20:03:09 -0800

    :sparkles: getting some values that might make sense coauthored with Gene and Nina

--- science/applications/co2_sensor_demo.cpp
+++ science/applications/co2_sensor_demo.cpp
@@ -17,12 +17,23 @@ using namespace std::chrono_literals;
 hal::status application(science::hardware_map& p_map) {
     // science robot entry point here. This function may yield an error.
     // configure drivers
-    hal::print<64>(*p_map.science_serial, "before while loop");
-    science::Co2Sensor co2_driver = science::Co2Sensor(*p_map.carbon_dioxide_sensor);
-    hal::print(*p_map.science_serial, "after initialization");
+    auto& carbon_dioxide_sensor = *p_map.carbon_dioxide_sensor;
+    auto& clock = p_map.clock;
+    // hal::print<64>(*p_map.science_serial, "before co2 initializng");
+
+    auto co2_driver = HAL_CHECK(science::Co2Sensor::create(carbon_dioxide_sensor, clock));
+    // hal::print<64>(*p_map.science_serial, "after co2 initializng");
+
+    // hal::print<64>(*p_map.science_serial, "before while loop");
+
+    // science::Co2Sensor co2_driver = science::Co2Sensor(carbon_dioxide_sensor, clock);
+
+    // hal::print(*p_map.science_serial, "after initialization");
     // hal::byte co2 = HAL_CHECK(co2_driver.read_co2());
 
     while (true) {
+        // HAL_CHECK(hal::delay(*p_map.clock, 50ms));
+
         // hal::print(*p_map.science_serial, "inside while loop");
         hal::byte co2 = HAL_CHECK(co2_driver.read_co2());
 
@@ -33,7 +44,7 @@ hal::status application(science::hardware_map& p_map) {
         
         hal::print<64>(*p_map.science_serial, "C02: %d\n", co2);
 
-        HAL_CHECK(hal::delay(*p_map.clock, 100ms));
+        HAL_CHECK(hal::delay(*p_map.clock, 50ms));
 
     }
 
--- science/implementations/co2_sensor.hpp
+++ science/implementations/co2_sensor.hpp
@@ -4,61 +4,73 @@
 #include <libhal/units.hpp>
 #include <span>
 #include <libhal-util/serial.hpp>
+#include <libhal-util/steady_clock.hpp>
 
 //i2cc address is 0x62
 //need a buffer
 //3 things to get from the i2c bus 
 //use std::span<hal::byte> for buffer to get data
 //use i2c bus 2 for the dev 2 board while testing 
+
+using namespace hal::literals;
+using namespace std::chrono_literals;
 namespace science {
 class Co2Sensor {
-    
+private:
+Co2Sensor(hal::i2c& p_i2c, hal::steady_clock* c): m_i2c(&p_i2c), clock{c} {
+
+        //start periodic measurement //write to hex 0x21b1
+        // hal::write(m_i2c, Addresses::address, std::span{a},hal::never_timeout());
+        // hal::write(m_i2c, Addresses::address, std::span{a});
+        // hal::transaction(sensor_address, start_address, nullptr, )
+    }  
     //constructor
 public:
     enum Addresses : hal::byte
     {
         address = 0x62,
-        start_first_half = 0xb1,
-        start_second_half = 0x21,
-        read_first_half = 0x05,
-        read_second_half = 0xec,
-        stop_first_half = 0x86,
-        stop_second_half = 0x3f 
-        // start_first_half = 0x21,
-        // start_second_half = 0xb1,
-        // read_first_half = 0xec,
-        // read_second_half = 0x05,
-        // stop_first_half = 0x3f,
-        // stop_second_half = 0x86 
+        // start_first_half = 0xb1,
+        // start_second_half = 0x21,
+        // read_first_half = 0x05,
+        // read_second_half = 0xec,
+        // stop_first_half = 0x86,
+        // stop_second_half = 0x3f 
+        start_first_half = 0x21,
+        start_second_half = 0xb1,
+        read_first_half = 0xec,
+        read_second_half = 0x05,
+        stop_first_half = 0x3f,
+        stop_second_half = 0x86 
     };
 
-    Co2Sensor(hal::i2c& p_i2c): m_i2c(p_i2c){
+    
+    static hal::result<Co2Sensor> create(hal::i2c& p_i2c, hal::steady_clock* c){
 
-        //start periodic measurement //write to hex 0x21b1
+        Co2Sensor co2sensor(p_i2c, c);
+        HAL_CHECK(co2sensor.start());
+        return co2sensor;
+    }
+
+    hal::status start(){
         std::array<hal::byte, 2> a{ {start_first_half, start_second_half} };
-        // hal::write(m_i2c, Addresses::address, std::span{a},hal::never_timeout());
-        hal::write(m_i2c, Addresses::address, a, hal::never_timeout());
-        // hal::write(m_i2c, Addresses::address, std::span{a});
-        // hal::transaction(sensor_address, start_address, nullptr, )
+        HAL_CHECK(hal::write(*m_i2c, Addresses::address, a, hal::never_timeout()));
+        return hal::success();
+
     }
     
-    hal::result<hal::byte> read_co2(){
+    hal::result<int32_t> read_co2(){
         // std::array<hal::byte,9> buffer; //3 bytes for co2, temperature and humidity
         std::array<const hal::byte, 2> burrito {read_first_half, read_second_half} ;
         
         std::array<hal::byte,9> buffer;
-        write_then_read(m_i2c, address, burrito, buffer, hal::never_timeout());
-        hal::print<64>(*p_map.science_serial, "first: %c", burrito[0]);
-        hal::print<64>(*p_map.science_serial, "second: %c", burrito[1]);
-        hal::print<64>(*p_map.science_serial, "third: %c", burrito[2]);
-        hal::print<64>(*p_map.science_serial, "fourth: %c", burrito[3]);
-        hal::print<64>(*p_map.science_serial, "fifth: %c", burrito[4]);
-        hal::print<64>(*p_map.science_serial, "sixth: %c", burrito[5]);
-        hal::print<64>(*p_map.science_serial, "seventh: %c", burrito[6]);
-        hal::print<64>(*p_map.science_serial, "eigth: %c", burrito[7]);
-        hal::print<64>(*p_map.science_serial, "ninth: %c", burrito[8]);
         
-        return buffer[0] << 8 | buffer[1];
+        // hal::write_then_read(m_i2c, Addresses::address, burrito, buffer, hal::never_timeout());
+        HAL_CHECK(hal::write(*m_i2c, Addresses::address, burrito));
+        hal::delay(*clock,50ms);
+
+        HAL_CHECK(hal::read(*m_i2c, Addresses::address, buffer, hal::never_timeout()));
+        return buffer[0] << 16 | buffer[1] << 8 | buffer[2];
+        // return buffer[0];
     }
 
 
@@ -68,14 +80,15 @@ public:
     hal::status stop(){
         std::array<hal::byte, 2> c{ {stop_first_half, stop_second_half} };
 
-        hal::write(m_i2c, Addresses::address, std::span{c},hal::never_timeout());
+        HAL_CHECK(hal::write(*m_i2c, Addresses::address, std::span{c},hal::never_timeout()));
     }
 
     //method to read 
     //reads 3 pieces of data: CO2 amount, temperature and humidity
     
-    hal::i2c& m_i2c;//initializing the i2c driver
+    hal::i2c* m_i2c;//initializing the i2c driver
     // hal::serial& serial;
+    hal::steady_clock* clock;
 
 };
 }
