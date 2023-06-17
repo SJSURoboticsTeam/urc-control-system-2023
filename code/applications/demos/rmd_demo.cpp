3ebb42ef514e338d518f2e216db3107d26c8e451
Coreyboy1820 <92711317+Coreyboy1820@users.noreply.github.com>
2023-06-17 12:40:31 -0700

    :sparkle: added demos and main control loops back

--- code/applications/demos/rmd_demo.cpp
+++ code/applications/demos/rmd_demo.cpp
@@ -8,9 +8,9 @@
 #include <libhal-util/steady_clock.hpp>
 #include <libhal/can.hpp>
 
-#include "../dto/drive_dto.hpp"
-#include "../hardware_map.hpp"
-#include "../drive/implementation/mission_control_handler.hpp"
+#include "drive/dto/drive_dto.hpp"
+#include "hardware_map.hpp"
+#include "drive/implementation/mission_control_handler.hpp"
 #include "common/util.hpp"
 
 hal::status application(sjsu::hardware_map& p_map)
@@ -18,7 +18,6 @@ hal::status application(sjsu::hardware_map& p_map)
   using namespace std::chrono_literals;
   using namespace hal::literals;
   Drive::drive_commands commands;
-  Drive::MissionControlHandler mission_control;
   auto& can = *p_map.can;
   auto& terminal = *p_map.terminal;
   auto& clock = *p_map.steady_clock;
@@ -45,15 +44,15 @@ hal::status application(sjsu::hardware_map& p_map)
 
   HAL_CHECK(hal::write(terminal, "Starting!\n"));
 
-  left_steer_motor.velocity_control(5_rpm);
-  right_steer_motor.velocity_control(5_rpm);
-  back_steer_motor.velocity_control(5_rpm);
-  left_hub_motor.velocity_control(5_rpm);
-  right_hub_motor.velocity_control(5_rpm);
-  back_hub_motor.velocity_control(5_rpm);
+  left_steer_motor.velocity_control(5.0_rpm);
+  right_steer_motor.velocity_control(5.0_rpm);
+  back_steer_motor.velocity_control(5.0_rpm);
+  left_hub_motor.velocity_control(5.0_rpm);
+  right_hub_motor.velocity_control(5.0_rpm);
+  back_hub_motor.velocity_control(5.0_rpm);
 
   while (true) {
-    HAL_CHECK(hal::delay(clock, 1s))
+    HAL_CHECK(hal::delay(clock, 1s));
   }
 
   return hal::success();
