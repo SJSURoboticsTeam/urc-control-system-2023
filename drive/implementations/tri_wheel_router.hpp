c01794de4f10fe1f0fe335631f4f11e1d358541d
Coreyboy1820 <92711317+Coreyboy1820@users.noreply.github.com>
2023-09-08 09:27:07 -0700

    Parsing commands currently works, just need to work out a bug which closes the connection

--- drive/implementations/tri_wheel_router.hpp
+++ drive/implementations/tri_wheel_router.hpp
@@ -37,97 +37,21 @@ public:
     return hal::success();
   }
 
-  /// At the moment, homing is where the legs turn on so we just calculate the
-  /// initial encoder positions. ***Must be called in main
-  // hal::status home(hal::steady_clock& counter)
-  // {
-  //   bool going_to_60 = false;
+  hal::result<motor_feedback> get_motor_feedback()
+  {
+    using namespace std::chrono_literals;
+    using namespace hal::literals;
+    motor_feedback motor_speeds;
+    
+    motor_speeds.left_drive_speed = HAL_CHECK(m_left.propulsion_speed_sensor->read()).speed;
+    motor_speeds.left_steer_speed = HAL_CHECK(m_left.steer_speed_sensor->read()).speed;
+    motor_speeds.back_drive_speed = HAL_CHECK(m_back.propulsion_speed_sensor->read()).speed;
+    motor_speeds.back_steer_speed = HAL_CHECK(m_back.steer_speed_sensor->read()).speed;
+    motor_speeds.right_drive_speed = HAL_CHECK(m_right.propulsion_speed_sensor->read()).speed;
+    motor_speeds.right_steer_speed = HAL_CHECK(m_right.steer_speed_sensor->read()).speed;
 
-  //   HAL_CHECK(left_.steer_motor_.position_control(0.0_deg, 2.0_rpm));
-  //   HAL_CHECK(hal::delay(counter, 10ms));
-  //   HAL_CHECK(right_.steer_motor_.position_control(0.0_deg, 2.0_rpm));
-  //   HAL_CHECK(hal::delay(counter, 10ms));
-  //   HAL_CHECK(back_.steer_motor_.position_control(0.0_deg, 2.0_rpm));
-
-  //   // max angle that the wheels will have to turn is 60 degrees for
-  //   // this step, so 2rpm is 720deg/min which is 12 deg/sec which
-  //   // means we need to wait 5 seconds to move 60 degrees but wait 6
-  //   // seconds to be safe
-  //   HAL_CHECK(hal::delay(counter, 6s));
-
-  //   // these are active high
-  //   bool leftPinLow = !(HAL_CHECK(left_.magnet_.level()).state),
-  //        rightPinLow = !(HAL_CHECK(right_.magnet_.level()).state),
-  //        backPinLow = !(HAL_CHECK(back_.magnet_.level()).state);
-
-  //   HAL_CHECK(hal::delay(counter, 10ms));
-
-  //   // TODO: put explanation for this V
-  //   // move them if 0 was home position due to inacuracy
-  //   if (leftPinLow) {
-  //     left_.wheel_offset_ = 60;
-  //     HAL_CHECK(left_.steer_motor_.position_control(60.0_deg, 2.0_rpm));
-  //     going_to_60 = true;
-  //   }
-  //   if (rightPinLow) {
-  //     right_.wheel_offset_ = 60;
-  //     HAL_CHECK(right_.steer_motor_.position_control(60.0_deg, 2.0_rpm));
-  //     going_to_60 = true;
-  //   }
-  //   if (backPinLow) {
-  //     back_.wheel_offset_ = 60;
-  //     HAL_CHECK(back_.steer_motor_.position_control(60.0_deg, 2.0_rpm));
-  //     going_to_60 = true;
-  //   }
-
-  //   // then if it is going to 60 we need to delay the same amount as
-  //   // the math above
-  //   if (going_to_60) {
-  //     HAL_CHECK(hal::delay(counter, 6s));
-  //   }
-
-  //   bool leftNotHome = true, rightNotHome = true, backNotHome = true;
-
-  //   while (leftNotHome || rightNotHome || backNotHome) {
-  //     if (leftNotHome) {
-  //       leftNotHome = HAL_CHECK(step(left_, counter));
-  //     }
-  //     if (rightNotHome) {
-  //       rightNotHome = HAL_CHECK(step(right_, counter));
-  //     }
-  //     if (backNotHome) {
-  //       backNotHome = HAL_CHECK(step(back_, counter));
-  //     }
-  //     HAL_CHECK(hal::delay(counter, 100ms));
-  //   }
-  //   return hal::success();
-  // }
-
-  // hal::result<motor_feedback> get_motor_feedback(hal::steady_clock& clock)
-  // {
-  //   using namespace std::chrono_literals;
-  //   using namespace hal::literals;
-  //   motor_feedback motor_speeds;
-  //   HAL_CHECK(
-  //     left_.steer_motor_.feedback_request(hal::rmd::drc::read::status_2));
-  //   HAL_CHECK(hal::delay(clock, 10ms));
-  //   HAL_CHECK(
-  //     right_.steer_motor_.feedback_request(hal::rmd::drc::read::status_2));
-  //   HAL_CHECK(hal::delay(clock, 10ms));
-  //   HAL_CHECK(
-  //     back_.steer_motor_.feedback_request(hal::rmd::drc::read::status_2));
-  //   HAL_CHECK(hal::delay(clock, 10ms));
-  //   // theory: I don't think rmds return speed through set position
-  //   // and only through set velocity, therefore we always have to
-  //   // request for feedback from the steers
-  //   motor_speeds.left_steer_speed = left_.steer_motor_.feedback().speed();
-  //   motor_speeds.right_steer_speed = right_.steer_motor_.feedback().speed();
-  //   motor_speeds.back_steer_speed = back_.steer_motor_.feedback().speed();
-  //   motor_speeds.left_drive_speed = left_.drive_motor_.feedback().speed();
-  //   motor_speeds.right_drive_speed = right_.drive_motor_.feedback().speed();
-  //   motor_speeds.back_drive_speed = back_.drive_motor_.feedback().speed();
-  //   return motor_speeds;
-  // }
+    return motor_speeds;
+  }
 
 private:
 
