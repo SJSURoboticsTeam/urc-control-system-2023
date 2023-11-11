563d818950a85154af37d8b70a8a51bccc755c8b
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2023-11-11 12:38:59 -0800

    transfer scd40.cpp

--- science/platform-implementations/scd40.cpp
+++ science/platform-implementations/scd40.cpp
@@ -4,16 +4,33 @@
 #include <libhal-util/serial.hpp>
 #include "scd40.hpp"
 
-scd40::create(hal::i2c& p_i2c, hal::steady_clock& clock){
-    scd40 scd40 = scd40(p_i2c, clock);
-    HAL_CHECK(scd40.start())
+class scd40 {
+    create(hal::i2c& p_i2c, hal::steady_clock& clock){
+        scd40 scd40 = scd40(p_i2c, clock);
+        HAL_CHECK(scd40.start())
 
+    }
+
+    start(){
+        std::array<hal::byte, 2> start_address =  { start_first_half, start_second_half };
+        HAL_CHECK(hal::write(m_i2c, device_address, start_address, hal::never_timeout()));
+        return hal::success();
+    }
+
+    std::array<hal::byte,9> read() {
+        std::array<const hal::byte, 2> read_address = {read_first_half, read_second_half };
+        std::array<hal::byte, 9> buffer;
+
+        HAL_CHECK(hal::write(m_i2c, device_address, read_address));
+        HAL_CHECK(hal::delay(steady_clock, 1ms));
+
+        HAL_CHECK(
+        hal::read(m_i2c, addresses::address, buffer, hal::never_timeout()));
+    }
+
+    get_CO2(){}
+
+    get_RH(){}
+
+    get_temp(){}
 }
-
-scd40::start(){}
-
-scd40::get_CO2{}
-
-scd40::get_RH(){}
-
-scd40::get_temp(){}
