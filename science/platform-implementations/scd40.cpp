a54c3887ed0e68208fc03aff77c1848e5ac717f2
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2023-11-11 13:39:43 -0800

    git scd40.cpp indentaiton

--- science/platform-implementations/scd40.cpp
+++ science/platform-implementations/scd40.cpp
@@ -2,33 +2,33 @@
 
 #include "scd40.hpp"
 
-    hal::result<scd40> scd40::create(hal::i2c& p_i2c, hal::steady_clock& clock){
-        scd40 scd40 = scd40(p_i2c, clock);
-        HAL_CHECK(scd40.start());
-        return scd40;
+hal::result<scd40> scd40::create(hal::i2c& p_i2c, hal::steady_clock& clock){
+    scd40 scd40 = scd40(p_i2c, clock);
+    HAL_CHECK(scd40.start());
+    return scd40;
 
-    }
+}
 
-    hal::status scd40::start(){
-        std::array<hal::byte, 2> start_address =  { start_first_half, start_second_half };
-        HAL_CHECK(hal::write(m_i2c, device_address, start_address, hal::never_timeout()));
-        return hal::success();
-    }
+hal::status scd40::start(){
+    std::array<hal::byte, 2> start_address =  { start_first_half, start_second_half };
+    HAL_CHECK(hal::write(m_i2c, device_address, start_address, hal::never_timeout()));
+    return hal::success();
+}
 
-    std::array<hal::byte,9> scd40::read() {
-        std::array<const hal::byte, 2> read_address = {read_first_half, read_second_half };
-        std::array<hal::byte, 9> buffer;
+std::array<hal::byte,9> scd40::read() {
+    std::array<const hal::byte, 2> read_address = {read_first_half, read_second_half };
+    std::array<hal::byte, 9> buffer;
 
-        HAL_CHECK(hal::write(m_i2c, device_address, read_address));
-        HAL_CHECK(hal::delay(steady_clock, 1ms));
+    HAL_CHECK(hal::write(m_i2c, device_address, read_address));
+    HAL_CHECK(hal::delay(steady_clock, 1ms));
 
-        HAL_CHECK(
-        hal::read(m_i2c, addresses::address, buffer, hal::never_timeout()));
-        return buffer;
-    }
+    HAL_CHECK(
+    hal::read(m_i2c, addresses::address, buffer, hal::never_timeout()));
+    return buffer;
+}
 
-    hal::result<double> scd40::get_CO2(){}
+hal::result<double> scd40::get_CO2(){}
 
-    hal::result<double> scd40::get_RH(){}
+hal::result<double> scd40::get_RH(){}
 
-    hal::result<double> scd40::get_temp(){}
+hal::result<double> scd40::get_temp(){}
