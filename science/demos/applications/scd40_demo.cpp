a2c75f75b8160d45520f0dad81cce4a11f2633df
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2023-11-11 16:13:49 -0800

    scd40 demo

---
+++ science/demos/applications/scd40_demo.cpp
@@ -0,0 +1,33 @@
+#include <libhal-armcortex/dwt_counter.hpp>
+#include <libhal-armcortex/startup.hpp>
+#include <libhal-armcortex/system_control.hpp>
+#include <libhal-util/serial.hpp>
+#include <libhal-util/steady_clock.hpp>
+#include <libhal/units.hpp>
+
+#include "../../platform-implementations/scd40.cpp"
+#include "../hardware_map.hpp"
+
+using namespace hal::literals;
+using namespace std::chrono_literals;
+namespace sjsu::science {
+
+hal::status application(application_framework& p_framework)
+{
+  // configure drivers
+  auto& i2c2 = *p_framework.i2c;
+  auto& clock = *p_framework.steady_clock;
+  auto& terminal = *p_framework.terminal;
+
+  auto scd40 = HAL_CHECK(science::scd40::create(i2c2, clock));
+
+  // while (true) {
+  //   hal::delay(clock, 500ms);
+  //   auto co2_level = HAL_CHECK(scd40.read_co2());
+  //   hal::print<64>(terminal, "C02: %d\n", co2_level);
+  //   hal::delay(clock, 1ms);
+  // }
+
+  return hal::success();
+}
+}  // namespace sjsu::science
