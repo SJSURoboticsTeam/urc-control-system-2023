76bfda0a630af696f4ac01779337d9a0e659437b
Kirthika Ashokkumar <kirthika@Kirthikas-MacBook-Pro-2.local>
2023-11-18 10:30:13 -0800

    print statments added to scd40_demo.cpp

--- science/demos/applications/scd40_demo.cpp
+++ science/demos/applications/scd40_demo.cpp
@@ -27,6 +27,19 @@ hal::status application(application_framework& p_framework)
   //   hal::print<64>(terminal, "C02: %d\n", co2_level);
   //   hal::delay(clock, 1ms);
   // }
+  while(true){
+        hal::delay(clock, 500ms);
+        auto co2_levels = HAL_CHECK(scd40_sensor.get_CO2());
+        auto temp = HAL_CHECK(scd40_sensor.get_temp());
+        auto RH_levels = HAL_CHECK(scd40_sensor.get_RH());
+
+        hal::print<64>(terminal, "CO2 Levels: %d\n", co2_levels);
+        hal::print<64>(terminal, "Temperature %d\n", temp);
+        hal::print<64>(terminal, "RH Levels: %d\n", RH_levels);
+
+        hal::delay(clock, 1ms);
+
+    }
 
   return hal::success();
 }
