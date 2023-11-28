5ece00ffe555788f8afb9425c06d0a4e23ca48db
vihashah <viha.shah@sjsu.edu>
2023-11-28 10:56:01 -0800

    start color sensor

---
+++ science/platform-implementations/color_sensor_opt4048.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <libhal-util/i2c.hpp>
+#include <libhal-util/serial.hpp>
+
+namespace science {
+    //functions we ened to make
+class opt4048
+{
+    private:
+    //constructor
+
+    public:
+    // select light range
+    //select conversion time
+    //measure light and color
+    //choose mode of operation
+    
+}
+}
