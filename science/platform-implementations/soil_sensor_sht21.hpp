8a31117738afee1c5501da3c32b27eb757d0b6f0
bork <bork.andrew.ryan@gmail.com>
2023-12-03 19:40:44 -0800

    Implemented sht21 driver

--- science/platform-implementations/soil_sensor_sht21.hpp
+++ science/platform-implementations/soil_sensor_sht21.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <libhal-util/i2c.hpp>
-#include <libhal-util/serial.hpp>
 
 
 namespace hal::sht {
@@ -9,32 +8,83 @@ namespace hal::sht {
     public:
       const static hal::byte sht21_i2c_address = 0x40;
 
-      static hal::result<sht21> create(hal::i2c * bus, hal::byte p_address = sht21_i2c_address);
+      /**
+       * @brief Resolution settings. Used with `set_resolution`
+       * rh_12bit_temp_14bit is 12 bits of resolution for relative humidity and 14 bit of resolution for temperature.
+       */
+      enum resolution : hal::byte {
+        rh_12bit_temp_14bit = 0b00000000,
+        rh_8bit_temp_12bit = 0b00000001,
+        rh_10bit_temp_13bit = 0b10000000,
+        rh_11bit_temp_11bit = 0b10000001,
+      };
+
+      /**
+       * @brief Create a sht21 sensor driver
+       * 
+       * @param p_bus I2C bus the device in on.
+       * @param p_address I2C address of the device
+       * @return hal::result<sht21> 
+       */
+      static hal::result<sht21> create(hal::i2c& p_bus, hal::byte p_address = sht21_i2c_address);
       
+      /**
+       * @brief Soft reset the sensor.
+       * 
+       * @return hal::status 
+       */
       hal::status soft_reset();
 
-      hal::result<bool> is_low_battery();
-      hal::status enable_heater(bool enabled = true);
-      hal::status enable_otp(bool enabled = true);
+      /**
+       * @brief Set the resolution of the relative humidity and temperature sensor.
+       * 
+       * @param p_resolution 
+       * @return hal::status 
+       */
+      hal::status set_resolution(resolution p_resolution);
 
+      /**
+       * @brief Returns true when VDD on the sensor is less than 2.25 V
+       * 
+       * @return hal::result<bool> 
+       */
+      hal::result<bool> is_low_battery();
+
+      /**
+       * @brief Enable or disable the on-chip heater.
+       * 
+       * @param p_enabled Defaults to true (enable)
+       * @return hal::status 
+       */
+      hal::status enable_heater(bool p_enabled = true);
+
+      /**
+       * @brief Perform a single relative humidity measurement. This will block until the measurement is ready.
+       * 
+       * @return hal::result<double> 
+       */
       hal::result<double> get_relative_humidity();
+
+      /**
+       * @brief Perform a single relative humidity measurement. This will block until the measurement is ready.
+       * 
+       * @return hal::result<double> 
+       */
       hal::result<double> get_temperature();
 
     private:
+      
+      enum command {
+        hold_temperature_measurement_command = 0b11100011,
+        hold_relative_humidity_measurement_command = 0b11100101,
+        no_hold_temperature_measurement_command = 0b11110011,
+        no_hold_relative_humidity_measurement_command = 0b11110101,
 
-      enum commands {
-        hold_temperature_measurement = 0b11100011,
-        hold_relative_humidity_measurement = 0b11100101,
-        no_hold_temperature_measurement = 0b11110011,
-        no_hold_relative_humidity_measurement = 0b11110101,
+        write_user_register_command = 0b11100110,
+        read_user_register_command = 0b11100111,
 
-        write_user_register = 0b11100110,
-        read_user_register = 0b11100111,
-
-        soft_reset = 0b11111110,
+        soft_reset_command = 0b11111110,
       };
-
-      hal::status execute_command(commands command);
 
       /**
        * @brief I2C Bus
@@ -46,6 +96,6 @@ namespace hal::sht {
        */
       hal::byte m_address;
   
-      sht21(hal::i2c * p_bus, hal::byte p_address);
+      sht21(hal::i2c& p_bus, hal::byte p_address);
   };
 };
