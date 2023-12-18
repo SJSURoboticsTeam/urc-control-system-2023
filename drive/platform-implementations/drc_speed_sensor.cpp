1416931ebbe213d68970a9df135db242ed87a9f7
Corey <coreyboy1820@hotmail.com>
2023-12-18 12:58:41 -0800

    fixing speed_sensor

--- drive/platform-implementations/drc_speed_sensor.cpp
+++ drive/platform-implementations/drc_speed_sensor.cpp
@@ -1,14 +1,7 @@
-#pragma once 
-
-#include "../src/speed_sensor.hpp"
 #include "../src/drc_speed_sensor.hpp"
 #include <libhal-rmd/drc.hpp>
 
 namespace sjsu::drive {
-
-    hal::result<drc_speed_sensor> make_speed_sensor(hal::rmd::drc& p_drc) {
-        return drc_speed_sensor(p_drc);
-    }
 
     drc_speed_sensor::drc_speed_sensor(hal::rmd::drc& p_drc) 
     : m_drc(&p_drc)
--- drive/platform-implementations/offset_servo.cpp
+++ drive/platform-implementations/offset_servo.cpp
@@ -17,7 +17,7 @@ void offset_servo::set_offset(hal::degrees p_offset) {
 offset_servo::offset_servo(hal::servo& p_servo, hal::degrees p_offset) : m_servo(&p_servo), m_offset(p_offset) {}
 
 
-hal::result<hal::servo::position_t> offset_servo::driver_position(hal::degrees p_position) override {
+hal::result<hal::servo::position_t> offset_servo::driver_position(hal::degrees p_position) {
     return HAL_CHECK(m_servo->position(p_position + m_offset));
 }
 
