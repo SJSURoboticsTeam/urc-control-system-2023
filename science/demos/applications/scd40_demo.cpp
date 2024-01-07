ff6590d79bab9c76581876f48e7f3509454b61f6
Kirthika Ashokkumar <kirthika@Kirthikas-MacBook-Pro-2.local>
2024-01-06 16:16:28 -0800

    Finished basic setting functions have not tested set_settings

--- science/demos/applications/scd40_demo.cpp
+++ science/demos/applications/scd40_demo.cpp
@@ -18,11 +18,18 @@ hal::status application(application_framework& p_framework)
   auto& i2c2 = *p_framework.i2c;
   auto& clock = *p_framework.steady_clock;
   auto& terminal = *p_framework.terminal;
-
+  
   auto scd40_sensor = HAL_CHECK(scd40::create(i2c2, clock));
 
   while(true){
+        // scd40_sensor.stop();
+        // auto get = HAL_CHECK(scd40_sensor.get_settings());
+        // auto temp = get.temp_offset;
+        // auto alt = get.altitude;
+        // hal::print<64>(terminal, "%-5.2f\t%-5.2f\n", temp, alt);
+
         // periodic readings are only updated every 5000ms (temperature)
+        
         hal::delay(clock, 5000ms);
         auto rd = HAL_CHECK(scd40_sensor.read());
         auto co2_levels = rd.co2;
@@ -30,11 +37,12 @@ hal::status application(application_framework& p_framework)
         auto RH_levels = rd.rh;
 
 
-        hal::print<64>(terminal, "%-5.2f\t%-5.2f\t%-5.2f\n", co2_levels, temp, RH_levels);
+        // hal::print<64>(terminal, "%-5.2f\t%-5.2f\t%-5.2f\n", co2_levels, temp, RH_levels);
+
         // hal::delay(clock, 500ms);
-        // hal::print<64>(terminal, "CO2 Levels: %f\n", co2_levels);
-        // hal::print<64>(terminal, "Temperature %f\n", temp);
-        // hal::print<64>(terminal, "RH Levels: %f\n", RH_levels);
+        hal::print<64>(terminal, "CO2 Levels: %f\n", co2_levels);
+        hal::print<64>(terminal, "Temperature %f\n", temp);
+        hal::print<64>(terminal, "RH Levels: %f\n", RH_levels);
 
     }
 
