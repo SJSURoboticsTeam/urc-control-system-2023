6f5417e1b5466397fcdb657fea72691f9af52f0e
hanley76129 <hanhuynh3422@gmail.com>
2024-02-05 10:56:49 -0800

    initial function names hpp

--- science/applications/science_state_machine.hpp
+++ science/applications/science_state_machine.hpp
@@ -1,4 +1,5 @@
- 
+#pragma once
+
 namespace sjsu::science::states{
     enum science_states{
         GET_SAMPLES,
@@ -6,6 +7,23 @@ namespace sjsu::science::states{
         BIURWT_TEST,
         OBSERVE_TEST
     };
+
+    hal::status pump_dio_water();
+
+    hal::status mix_solution();
+
+    hal::status pump_to_vial();
+    
+    hal::status pump_reagents();
+
+    hal::status rotate_vials();
+
+    hal::status turn_on_light();
+    
+    hal::status read_color_sensor();
+
+
+
 
     
 
