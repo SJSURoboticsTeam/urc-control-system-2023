030968cdebb56ad9445b6a82a4b111094c792156
hanley76129 <hanhuynh3422@gmail.com>
2024-02-17 13:51:49 -0800

    added position tracker and additional details to the states

--- science/applications/science_state_machine.cpp
+++ science/applications/science_state_machine.cpp
@@ -1,41 +1,56 @@
 #include "science_state_machine.hpp"
-#include "revolver.hpp"
+#include "revolver.cpp"
 #include <libhal/servo.hpp> 
 namespace sjsu::science{
-    hal::status science_state_machine::run_state_machine(science_states state){
-        switch(state){
+    hal::result<science_states> science_state_machine::run_state_machine(science_states state){
+        switch(current_state){
             case science_state_machine::science_states::GET_SAMPLES:
                 pump_dio_water();
-                
                 mix_solution();
-                pump_to_vial();
-
-                rotate_vial_to_position()
-                pump_to_vial(); 
-                break;
+                pump_to_vial(1);
+                revolverMoveVials(1); // move to vial 2
+                pump_to_vial(2); 
+                current_state= science_states::MOLISCH_TEST;
+                break; 
             case science_state_machine::science_states::MOLISCH_TEST:
-                pump_reagents();
-                rotate_vial_to_position()
-                turn_on_light();
-                rotate_vial_position(); 
-                read_color_sensor(); 
+                pump_reagents(1);
+                current_position = vial2_position::MOLISCH; 
+                revolverMoveVials(2); //move vial 1 to camera/color sensor 
+                current_position= vial2_position::BIURET; 
+                up_and_down(); //moves color sensor up and down so it can read the color
+                read_color_sensor(); //could be removed
+                if(read_color_sensor_purple()){
+                    current_state= science_states::BIURET_TEST;
+                }
+                else{
+                    current_state= science_states::GET_SAMPLES;
+                }
                 break;
             case science_state_machine::science_states::BIURET_TEST:
-                pump_reagents();
-                rotate_vial_to_position(); 
-                turn_on_light();
-                rotate_vial_position();
+                pump_reagents(2); // pump into vial 2
+                revolverMoveVials(1); // rotate to camera/color_sensor 
+                current_position= vial2_position::CAMERA;
+                up_and_down(); 
                 read_color_sensor(); 
+                current_state= science_states::RESET;
                 break;
             case science_state_machine::science_states::RESET:
-                rotate_vial_to_position(); 
+                if((vial2_position-2)<0){
+                    revolverMoveVials(1);
+                }
+                else{
+                    revolverMoveVials(-(vial2_position-2));
+                }
+                current_state= science_states::GET_SAMPLES;
                 break; 
         }
-        //check if you continue and containment reset 
+        return state; 
+        
+    }
+    hal::status science_state_machine::containment_reset(){
+        turn_off_pumps(); 
+        state=science_states::GET_SAMPLES; 
     }
 }
-//check substates
-//exit if molish test fails 
-//
 
 
--- science/applications/science_state_machine.hpp
+++ science/applications/science_state_machine.hpp
@@ -9,21 +9,26 @@ namespace sjsu::science{
             BIURET_TEST,
             RESET
         };
-
-        hal::result<bool> check_sample(); 
-        hal::status run_state_machine(science_states state);
+        enum vial2_position{
+            SAMPLE=1, 
+            MOLISCH, 
+            SULFURIC, 
+            BIURET,
+            CAMERA
+        };
+        positions current_position= SAMPLE; 
+        science_states current_state= GET_SAMPLES; 
+        hal::status run_state_machine(science_states current_state);
         hal::status pump_dio_water();
         hal::status mix_solution();
         hal::status pump_to_vial();
         hal::status pump_reagents();
-        hal::status rotate_vials();
-        hal::status turn_on_light();
-        hal::status read_color_sensor();//move contnets insdie
-        hal::result<bool> get_mission_control_cmnd(); //temporary
         hal::status containment_reset();
+        hal::status up_and_down(); 
+        hal::status containment_reset(); 
+        hal::status turn_off_pumps();
 
-
-        
+    
 
         
 
