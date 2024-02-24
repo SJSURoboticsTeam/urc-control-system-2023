220b6e28a8f1648bab8daff4365bad6bac5120da
bork <bork.andrew.ryan@gmail.com>
2024-02-23 16:36:33 -0800

    Moved config to settings.hpp

--- drive/include/settings.hpp
+++ drive/include/settings.hpp
@@ -1,7 +1,24 @@
 #pragma once
 
+#include "drive_configuration.hpp"
 namespace sjsu::drive {
     // Multiply this with the desired angle to get the correct angle to steer the motors.
     constexpr float angle_correction_factor = 1.3625; // ???? WHY ARE THESE MOTORS SCALED????
+    
     constexpr float k_max_speed = 100;
+
+    // These control how sensitive the system is to changes in the target.
+    drive_configuration config_kP = {
+        .steering_angle = 1,
+        .wheel_heading = 10,
+        .wheel_speed = 20,
+    };
+
+    // These control how fast the system can react to changes in the target.
+    drive_configuration config_max_delta = {
+        .steering_angle = 40,
+        .wheel_heading = 360,
+        .wheel_speed = 50,
+    };
+
 };
