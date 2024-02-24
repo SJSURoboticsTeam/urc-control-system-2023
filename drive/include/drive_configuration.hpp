68c5d954805c08188816b26186d06284f5ccb516
bork <bork.andrew.ryan@gmail.com>
2024-02-23 16:16:45 -0800

    Added drive_configuration default constructor

--- drive/include/drive_configuration.hpp
+++ drive/include/drive_configuration.hpp
@@ -8,6 +8,7 @@ namespace sjsu::drive {
         float wheel_heading = 0.0;
         float wheel_speed = 0.0;
 
+        constexpr drive_configuration() {}
         constexpr drive_configuration(float p_steering_angle, float p_wheel_heading, float p_wheel_speed) : 
             steering_angle(p_steering_angle),
             wheel_heading(p_wheel_heading),
