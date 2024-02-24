14cbd0f2f1f488876009acf6e1bd74d3915969a3
bork <bork.andrew.ryan@gmail.com>
2024-02-23 16:37:25 -0800

    added constexpr to settings

--- drive/include/settings.hpp
+++ drive/include/settings.hpp
@@ -8,14 +8,14 @@ namespace sjsu::drive {
     constexpr float k_max_speed = 100;
 
     // These control how sensitive the system is to changes in the target.
-    drive_configuration config_kP = {
+    constexpr drive_configuration config_kP = {
         .steering_angle = 1,
         .wheel_heading = 10,
         .wheel_speed = 20,
     };
 
     // These control how fast the system can react to changes in the target.
-    drive_configuration config_max_delta = {
+    constexpr drive_configuration config_max_delta = {
         .steering_angle = 40,
         .wheel_heading = 360,
         .wheel_speed = 50,
