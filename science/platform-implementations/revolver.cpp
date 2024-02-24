9b6c91cf82cbbd184e58b90959fb1e45baa5a262
shychai <raj.brayana@gmail.com>
2024-02-24 14:41:15 -0800

    Commiting only revolver files for now

--- science/platform-implementations/revolver.cpp
+++ science/platform-implementations/revolver.cpp
@@ -1,24 +1,68 @@
 #pragma once
 #include "revolver.hpp"
-
+#include <libhal-util/steady_clock.hpp>
+#include <libhal-lpc40/clock.hpp>
+#include <libhal-soft/rc_servo.hpp>
+#include <libhal-lpc40/pwm.hpp>
+#include <libhal-pca/pca9685.hpp>
+#include <libhal-lpc40/input_pin.hpp>
+#include <libhal-util/units.hpp>
 namespace sjsu::science {
+
+    revolver::revolver(hal::servo& p_servo, hal::input_pin& p_input_pin, hal::steady_clock& p_steady_clock)
+    {
+        revolver::create(p_servo, p_input_pin, p_steady_clock);
+    }
     
-    revolver::revolver(hal::motor& p_motor, hal::input_pin& p_hall_effect_input_pin): 
-        motor(p_motor),
-        hall_effect_input_pin(p_hall_effect_input_pin) {}
-
-    hal::result<revolver> revolver::create(hal::motor& p_motor, hal::input_pin& p_hall_effect_input_pin) {
-        return revolver(p_motor,p_hall_effect_input_pin);
+    hal::result<revolver> revolver::create(hal::servo& p_servo, hal::input_pin& p_input_pin, hal::steady_clock& p_steady_clock) 
+    {
+        revolver_servo_my = p_servo;
+        input_pin_my = p_input_pin;
+        steady_clock_my = p_steady_clock;
     }
-    hal::status revolver::rotate_vial_to_position(revolver::positions position, int vial) {
 
+    hal::status revolver::revolverMove(hal::degrees rotation) 
+    {
+        HAL_CHECK(revolver_servo_my.position(rotation));
+    }
+
+    hal::status revolver::revolverStop()
+    {
+        HAL_CHECK(revolver_servo_my.position(hal::degrees(360.0)));
+    }
+
+    hal::status revolver::revolverMoveVials(int vial) 
+    {
+        int count = 0;
+        hal::degrees rotation = hal::degrees(0.0); //clockwise
+
+        if (vial < 0)
+        {
+            vial = -vial; 
+            rotation = hal::degrees(180.0); //counterclockwise
+        }
+
+        if (vial <= m_numVials && vial != 0)
+        {
+            revolverMove(rotation);
+
+            while (count < vial) 
+            {
+                bool state = input_pin_my.level().value().state;
+
+                hal::delay(steady_clock_my, std::chrono::milliseconds(5.0));
+
+                bool stateDelay = input_pin_my.level().value().state;
+
+                if (state != stateDelay)
+                {
+                    count = count + 1;
+                }
+            }
+
+            revolverStop();
+        }
         
-
-    }
-    hal::status revolver::move_one(bool forward) {
-        motor.power(k_motor_rotate_power);
-        while (hall_effect_input_pin.level()) {}
-    }
-        
+    }     
 
 }
--- science/platform-implementations/revolver.hpp
+++ science/platform-implementations/revolver.hpp
@@ -1,17 +1,26 @@
 #pragma once
-#include <libhal/motor.hpp>
-#include <libhal/input_pin.hpp>
+#include <libhal-util/steady_clock.hpp>
+#include <libhal-lpc40/clock.hpp>
+#include <libhal-soft/rc_servo.hpp>
+#include <libhal-lpc40/pwm.hpp>
+#include <libhal-pca/pca9685.hpp>
+#include <libhal-lpc40/input_pin.hpp>
+#include <libhal-util/units.hpp>
 
 namespace sjsu::science {
 
     class revolver {
         private:
-            static const float k_motor_rotate_power = 0.01;
-            int position = 0; //vial in sample position
-            hal::motor& motor;
-            hal::input_pin& hall_effect_input_pin;
-            revolver(hal::motor& p_motor, hal::input_pin& p_hall_effect_input_pin);
-            hal::status move_one(bool forward);
+            int m_position = 0; //vial in sample position
+            int m_numVials = 12;
+
+            hal::servo& revolver_servo_my;
+            hal::input_pin& input_pin_my;
+            hal::steady_clock& steady_clock_my;
+
+            hal::status revolverClockwise();
+            hal::status revolverCounterClockwise();
+            hal::status revolverStop();
 
         public:
             enum class positions{
@@ -21,8 +30,9 @@ namespace sjsu::science {
                 BIURET_REAGENT,
                 OBSERVATION
             };
-            static hal::result<revolver> create(hal::motor& p_motor, hal::input_pin& p_hall_effect_input_pin);
-            hal::status rotate_vial_to_position(positions position, int vial);
-    };
 
+            static hal::result<revolver> create();
+                                                
+            hal::status revolverMoveVials(int vial);
+    };
 }
