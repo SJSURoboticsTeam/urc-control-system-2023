bf2b19f5a65d30937bf8930b65f445475d100642
shychai <raj.brayana@gmail.com>
2024-03-02 08:41:13 -0800

    Updated to test delay based on user input

--- science/demos/applications/rotunda_science.cpp
+++ science/demos/applications/rotunda_science.cpp
@@ -8,6 +8,7 @@
 
 #include "../hardware_map.hpp"
 
+#include <string>
 #include <string_view>
 #include <array>
 #include <cinttypes>
@@ -32,21 +33,57 @@ namespace sjsu::science
         auto& rotunda_science_my = *p_framework.rotunda_science;
         auto& magnet_my = *p_framework.magnet;
 
+        hal::degrees m_clockwise = hal::degrees(0.0);
+        hal::degrees m_counterclockwise = hal::degrees(180.0);
+        hal::degrees m_stop = hal::degrees(360.0);
 
-        while (true) 
+        int count = -1; // account for initial state change
+        bool hallState;
+        bool hallStateDelayed;
+
+        std::array<hal::byte, 1024> received_buffer{};
+
+        while (true)
         {
-            hal::delay(clock,std::chrono::milliseconds(5));
+            auto readings = HAL_CHECK(terminal.read(received_buffer));
+            auto result = to_string_view(received_buffer);	
 
-            if (magnet_my.level().value().state)
+            if (result.find("{") == std::string_view::npos)
             {
-                hal::print<1024>(terminal,"2 slow 2 sad\n");
-                HAL_CHECK(rotunda_science_my.position(hal::degrees(360.0)));
+                hal::print<1024>(terminal,"Awaiting User Input: \n");
             }
-            else 
+            else
             {
-                hal::print<1024>(terminal,"2 fast 2 furious\n");
-                HAL_CHECK(rotunda_science_my.position(hal::degrees(180.0)));
-            }
+                result = result.substr(result.find('{') + 1 );
+
+                // int vial = (std::clamp(std::stoi(result.data()),-m_numVials,m_numVials));
+                int vial = 6;
+                std::chrono::milliseconds m_delay = std::chrono::milliseconds(std::stoi(result.data()));
+
+                HAL_CHECK(rotunda_science_my.position((vial > 0) ? m_clockwise : m_counterclockwise));
+                hal::print<1024>(terminal, "User Inputted: %s\n",readings.data);
+                hal::print<1024>(terminal, "Revolver Start, Moving %d Vials\n", vial);
+                vial = (vial > 0) ? vial : -vial;
+
+                while (count < vial) 
+                {
+                    hallState = magnet_my.level().value().state;
+                    hal::delay(clock,std::chrono::milliseconds(m_delay));
+                    hallStateDelayed = magnet_my.level().value().state;
+                    
+                    if (hallState != hallStateDelayed)
+                    {
+                        ++count;
+                        hal::print<1024>(terminal, "Vials Passed: %d\n", count);
+                    }
+                }
+                
+                HAL_CHECK(rotunda_science_my.position(m_stop));
+                hal::print<1024>(terminal, "Revolver Stop");
+                
+                count = -1;
+                result = std::string_view();
+            } 
         }
-    } 
+    }
 }
