e760cdb3d362b3db87c6f2a229830986fcc994a6
Nina Wang <115374853+ninaw04@users.noreply.github.com>
2024-04-22 16:33:23 -0700

    :sparkles: Mimic calibration (#132)
    * :zap: calibrated shoulder and elbow of mimic
    
    * :construction: mimic undergoing tests
    
    * :stars: calibrated shoulder and elbow to current mimic
    
    * :sparkles: mimic worksgit add .

--- arm_mimic/applications/application.cpp
+++ arm_mimic/applications/application.cpp
@@ -28,7 +28,8 @@ hal::status application(application_framework& p_framework)
 
     const uint8_t N = 6;
     std::array<float, N> scrambled_results = {};
-    std::array<float, N> results = {};
+    std::array<float, N> results = {0};
+    char* strings[] = {"Rotunda", "Shoulder", "Elbow", "Wrist Pitch", "Wrist Roll", "EndE"};
     float true_degree;
     float max_volt = 4096;
     float max_deg = 360;
@@ -41,18 +42,48 @@ hal::status application(application_framework& p_framework)
         for(int i = 0; i < N; i++) {
             true_degree = tla::voltage_to_degree(buffer_of_results[i], max_volt,max_deg);
             scrambled_results[i] = true_degree;
-            // hal::print<64>(uart0, "voltage %d: %d ", i, (int)true_degree); 
+            // scrambled_results[i] = buffer_of_results[i];
+            // hal::print<64>(uart0, "volt %d: %d ", i, (int)buffer_of_results[i]);
         }
         // hal::print<64>(uart0, "\n");
 
-        results[0] = 0;                     // ROTUNDA
-        results[1] = scrambled_results[2] - 261;  // SHOULDER
-        results[2] = -(scrambled_results[1] - 151);  // ELBOW
-        // results[3] = scrambled_results[0];  // WRIST PITCH
-        // results[4] = scrambled_results[5];  // WRIST ROLL
-        results[3] = 0;
-        results[4] = 0;
-        results[5] = 0;
+        // for(int i = 0; i < N; i++) {
+        //     hal::print<64>(uart0, "volt %d: %d ", i, (int)scrambled_results[i]);
+        // }
+        // hal::print<64>(uart0, "\n");
+
+        // sr1 is shoulder
+
+        // memset(0, results, sizeof(results));
+
+        results[0] = (8*(scrambled_results[0] - 178) - 50);                     // ROTUNDA
+        results[1] = -(scrambled_results[1] - 346);
+        results[2] = -(scrambled_results[2] - 155);
+        results[3] = (scrambled_results[5] - 180)/2;
+        results[4] = scrambled_results[4] - 180;
+        results[5] = scrambled_results[3]/3;
+
+        // HARD LOCKS
+        if(results[0] > 80) { // ROTUNDA
+            results[0] = 80;
+        } else if (results[0] < -80) {
+            results[0] = -80;
+        }
+
+        if(results[2] > 85) {
+            results[0] = 85;
+        }
+
+        if(results[5] > 100) {
+            results[5] = 100;
+        }
+
+        // HARD STOP ELBOW 85
+
+        // for(int i = 0; i < N; i++) {
+        //     hal::print<64>(uart0, "%s:%d  ", strings[i], (int)results[i]);
+        // }
+        // hal::print<64>(uart0, "\n");
 
         HAL_CHECK(tla::send_data_to_mc(*p_framework.terminal, results));
     }
